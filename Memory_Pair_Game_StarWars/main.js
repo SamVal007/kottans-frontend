4d8441550ebdecf351da66f9e14203ba4eea9ffe
SamVal <samoylenkovalera88@gmail.com>
2021-02-25 04:08:12 +0200

    fixes

--- Memory_Pair_Game_StarWars/main.js
+++ Memory_Pair_Game_StarWars/main.js
@@ -1,22 +1,31 @@
 let similarCards = [];
 let blockedClick = false;
 const images = ['Dart_Weider', 'lyuk-skajuoker', 'R2-D2', 'c-3po',
-                'printsessa-leya-organa', 'enakin-skajuoker', 'obi-van-kenobi',
-                 'dart-sidius', 'kvaj-gon-dzhinn','joda'];
+     'printsessa-leya-organa', 'enakin-skajuoker', 'obi-van-kenobi',
+     'dart-sidius', 'kvaj-gon-dzhinn', 'joda'
+];
+const flippDelay = 900,
+    hiddingDelay = 600,
+    victoryMessageDelay = 1200,
+    enoughCountOfSimilarCards = 2;
 
 const mainAttributes = {
     container: 'container',
-    game: 'game',
+    cards: 'cards',
     card: 'card',
     front: 'front_card',
     frontImg: 'img-front_card',
     back: 'back_card',
     backImg: 'img-back_card',
     flipped: 'flipped',
-    hidden: 'hidden'
+    hidden: 'hidden',
+
+    imgBackLogoUrl: 'img/back_logo.png',
+    imgBackLogoAlt: 'logo_card',
+    imgFrontLogoUrl: 'img/'
 };
 
-const cleanCountArray = () => similarCards = []; //similarCards.length = 0;
+const cleanCountArray = () => similarCards = [];
 
 const flippSelectedCards = () => {
     setTimeout(() => {
@@ -25,7 +34,7 @@ const flippSelectedCards = () => {
             cleanCountArray();
             blockedClick = false;
         });
-    }, 900);
+    }, flippDelay);
 };
 
 const hideSimilarCards = () => {
@@ -34,25 +43,19 @@ const hideSimilarCards = () => {
             card.classList.add(mainAttributes.hidden);
             cleanCountArray();
             blockedClick = false;
-            const counterHiddenCards = document.querySelectorAll(".hidden").length;
-            const amountOfCards = (images.length * 2);
-            // console.log(amountOfCards);
-            // console.log(typeof(amountOfCards));
-            // console.log(counterHiddenCards);
-            // console.log(typeof(counterHiddenCards));
 
-            if (counterHiddenCards === amountOfCards) {
-                victoryMessage();
-            }
+            checkingForTheVictory();
+
         });
-    }, 600);
+    }, hiddingDelay);
 };
 
 const compareSimilarCards = () => {
     const countCards = similarCards.length;
-    if (countCards == 2) {
+    if (countCards === enoughCountOfSimilarCards) {
         blockedClick = true;
-        if (similarCards[0] === similarCards[1]) {
+        const [firstCard, secondCard] = similarCards;
+        if (firstCard === secondCard) {
             hideSimilarCards();
         } else {
             flippSelectedCards();
@@ -64,19 +67,18 @@ const selectCard = ({target}) => {
     if (!blockedClick) {
         const selectedCard = target.closest('.card');
         if (selectedCard && !selectedCard.classList.contains('flipped')) {
-            let alt = target.parentNode.nextSibling.firstChild.getAttribute("alt");
+            let cadName = target.getAttribute("id");
             selectedCard.classList.add(mainAttributes.flipped);
-            similarCards.push(alt);
+            similarCards.push(cadName);
             compareSimilarCards();
         }
     }
 };
 
 const drawCards = () => {
-    const fragment = [];
 
-    const game = document.createElement('div');
-    game.classList.add(mainAttributes.game);
+    const cards = document.createElement('div');
+    cards.classList.add(mainAttributes.cards);
 
     const arrImages = [...images, ...images];
     const shuffleArray = arrImages.sort(function () {
@@ -90,35 +92,51 @@ const drawCards = () => {
         const frontImg = document.createElement('img');
         const backImg = document.createElement('img');
 
-        card.classList.add(mainAttributes.card);
-        frontCard.classList.add(mainAttributes.front);
-        backCard.classList.add(mainAttributes.back);
-        frontImg.classList.add(mainAttributes.frontImg);
-        frontImg.src = `img/${item}.png`;
-        frontImg.alt = `${item}`;
-        backImg.classList.add(mainAttributes.backImg);
-        backImg.src = 'img/back_logo.png';
-        backImg.alt = 'logo_card';
-
         frontCard.append(frontImg);
         backCard.append(backImg);
         card.append(backCard);
         card.append(frontCard);
-        game.append(card);
-    });
-    fragment.push(game);
+        cards.append(card);
 
-    return fragment;
+        card.classList.add(mainAttributes.card);
+        frontCard.classList.add(mainAttributes.front);
+        backCard.classList.add(mainAttributes.back);
+        frontImg.classList.add(mainAttributes.frontImg);
+        frontImg.src = mainAttributes.imgFrontLogoUrl + `${item}.png`;
+        frontImg.alt = `${item}`;
+        backImg.classList.add(mainAttributes.backImg);
+        backImg.src = mainAttributes.imgBackLogoUrl;
+        backImg.alt = mainAttributes.imgBackLogoAlt;
+        backImg.id = frontImg.alt;
+
+        let template =
+            `<div >
+            <div class="picture">
+              <img src="${item}">
+            </div>
+          </div>`;
+
+    });
+
+    return cards;
 };
 
 const showCards = () => {
     const body = document.querySelector('body');
     const container = document.createElement('div');
     container.classList.add(mainAttributes.container);
-    container.append(...drawCards());
+    container.append(drawCards());
     container.addEventListener("click", selectCard);
     body.append(container);
 };
+
+const checkingForTheVictory = () => {
+    const counterHiddenCards = document.querySelectorAll(".hidden").length;
+    const amountOfCards = ((images.length) * enoughCountOfSimilarCards);
+    if (counterHiddenCards === amountOfCards) {
+        victoryMessage();
+    }
+}
 
 const victoryMessage = () => {
     setTimeout(() => {
@@ -126,7 +144,7 @@ const victoryMessage = () => {
             document.body.innerHTML = "";
             showCards();
         } else window.close();
-    }, 1200);
+    }, victoryMessageDelay);
 };
 
 window.onload = () => {
@@ -134,4 +152,3 @@ window.onload = () => {
         showCards();
     } else window.close();
 };
-
